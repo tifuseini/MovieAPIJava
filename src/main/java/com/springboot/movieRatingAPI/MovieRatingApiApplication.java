266cdac516a66c42f6aee19337ad2b683b645741
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-03 11:30:00 +0000

    Source\

---
+++ src/main/java/com/springboot/movieRatingAPI/MovieRatingApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.movieRatingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/movieRatingAPI/MovieRatingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.movieRatingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
